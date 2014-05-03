1b30cc4e68c84463a89abb82653df276b942a1e7
Wilson Peng <wilsonpeng8@gmail.com>
2014-05-03 10:18:19 -0700

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Didyouknow::Application.config.secret_key_base = 'dd0fe1c527005241c8906ab2eb873e23a13b1e12fc4ff3470c6a355e7282951340680dcfabc1edcac520d87d6b451de689758fa350dc2e91601c7f9fdba6a349'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Didyouknow::Application.config.session_store :cookie_store, key: '_didyouknow_session'
